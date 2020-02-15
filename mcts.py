248bf2512baafee3c765ad32e6e7daeb2884da0a
Henning Sperr <henning.sperr@gmail.com>
2020-02-15 14:13:24 +0100

    EP 15: Improve MCTS

--- mcts.py
+++ mcts.py
@@ -1,16 +1,18 @@
 import time
 import random
+import math
 
 class MonteCarloTreeSearch:
 
     def __init__(self):
         self.states = []
-        self.max_moves = 50
+        self.max_moves = 100000
 
         self.plays = {}
         self.wins = {}
 
     def get_best_move_time(self, board, allowed_time_in_s, show_perft=False):
+        print(f"MCTS with time={allowed_time_in_s} and max_moves={self.max_moves}")
         poss_moves = board.possible_moves()
         if not poss_moves:
             return
@@ -23,15 +25,12 @@ class MonteCarloTreeSearch:
             self._run_simulation(board.copy())
             games += 1
 
-        next_states = []
         player = board.current_player
-        for move in poss_moves:
-            board.move(*move)
-            next_states.append((board.to_string(), move))
-            board.undo()
+        board_string = board.to_string()
+        next_states = [(player, board_string, move) for move in poss_moves]
 
-        rates = sorted((self.wins.get((player, S), 0) / float(self.plays.get((player, S), 0)), m, self.plays.get((player, S), 0)) for S, m in next_states)
         print(games)
+        rates = sorted((self.wins.get(S, 0) / float(self.plays.get(S, 1)), S[-1], self.plays.get(S, 0)) for S in next_states)
         for pct_win, move, plays in rates:
             print(pct_win, move, plays)
 
@@ -43,41 +42,54 @@ class MonteCarloTreeSearch:
         expand = True
         visits = []
 
+        plays, wins = self.plays, self.wins
+
         player = board.current_player
         for m in range(self.max_moves):
             poss_moves = board.possible_moves()
-            move = random.choice(poss_moves)
+
+            board_string = board.to_string()
+            next_states = [(player, board_string, move) for move in poss_moves]
+            if all(state in plays for state in next_states):
+                next_moves = []
+                for state in next_states:
+                    _, _, move = state
+                    s_i = plays[state]
+                    w_i = wins[state]
+                    s_p = sum(plays[s] for s in next_states)
+                    r = w_i/s_i + 1.4 * math.sqrt(math.log(s_p)/s_i)
+                    next_moves.append((r, move))
+
+                _, move = sorted(next_moves)[-1]
+            else:
+                move = random.choice(poss_moves)
+
+            state = (player, board.to_string(), move)
             board.move(*move)
 
-
-            state = (player, board.to_string())
-            if expand and not state in self.plays:
+            if expand and not state in plays:
                 expand = False
-                self.plays[state] = 0
-                self.wins[state] = 0
+                plays[state] = 0
+                wins[state] = 0
 
-
-            if state in self.plays:
+            if state in plays:
                 visits.append(state)
             player = board.current_player
             winner = board.winning_player()
             if winner:
                 break
 
-
-        if board.boxes_for(board.current_player)>board.boxes_for(board.other_player):
+        if board.boxes_for(board.current_player) > board.boxes_for(board.other_player):
             winner = board.current_player
-        elif board.boxes_for(board.current_player)<board.boxes_for(board.other_player):
+        elif board.boxes_for(board.current_player) < board.boxes_for(board.other_player):
             winner = board.other_player
         else:
             winner = 0
 
-
-
         for state in visits:
-            self.plays[state] += 1
+            plays[state] += 1
             if winner == state[0]:
-                self.wins[state] += 1
+                wins[state] += 1
 
 if __name__=='__main__':
     from board import Board
